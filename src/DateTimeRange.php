e73d7da1b3f52b8ff8ed128afda31aaf62761786
daffox <daffox@users.noreply.github.com>
2016-04-17 17:05:24 +0300

    Added files via upload

--- src/DateTimeRange.php
+++ src/DateTimeRange.php
@@ -4,9 +4,6 @@
  * This file is part of the Carbon package.
  *
  * (c) Yury Apsitis <yury.apsitis.dev@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
  */
 
 namespace DateTimeRange;
@@ -15,7 +12,9 @@ use DateTime;
 
 class DateTimeRange {
     
+    /** @var \DateTime beginning of range */
     protected $range_starts = null;
+    /** @var \DateTime end of range */
     protected $range_ends   = null;
     
     /**
